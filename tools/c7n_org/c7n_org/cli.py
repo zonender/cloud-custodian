0b76b04aa4706fd080b8ec71f2d265b4c45090af
mattboret <mattboret@gmail.com>
2020-11-11 12:09:27 +0100

    tools/c7n-org - fix config parsing error with aws profile (#6281)

--- tools/c7n_org/c7n_org/cli.py
+++ tools/c7n_org/c7n_org/cli.py
@@ -488,9 +488,10 @@ def run_script(config, output_dir, accounts, tags, region, echo, serial, script_
 
 def accounts_iterator(config):
     for a in config.get('accounts', ()):
-        if isinstance(a['role'], str) and not a['role'].startswith('arn'):
-            a['role'] = "arn:aws:iam::{}:role/{}".format(
-                a['account_id'], a['role'])
+        if 'role' in a:
+            if isinstance(a['role'], str) and not a['role'].startswith('arn'):
+                a['role'] = "arn:aws:iam::{}:role/{}".format(
+                    a['account_id'], a['role'])
         yield {**a, **{'provider': 'aws'}}
     for a in config.get('subscriptions', ()):
         d = {'account_id': a['subscription_id'],
