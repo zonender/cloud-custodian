be131c2625e4e5fda38db51476520daaf6a18a3e
h-dub <72996437+h-dub@users.noreply.github.com>
2020-12-28 10:40:59 -0800

    azure - add firewall filters to postgresql servers. (#6357)

--- tools/c7n_azure/c7n_azure/resources/postgresql_server.py
+++ tools/c7n_azure/c7n_azure/resources/postgresql_server.py
@@ -3,6 +3,10 @@
 
 from c7n_azure.provider import resources
 from c7n_azure.resources.arm import ArmResourceManager
+from c7n_azure.filters import FirewallRulesFilter
+from netaddr import IPRange, IPSet
+
+AZURE_SERVICES = IPRange('0.0.0.0', '0.0.0.0')
 
 
 @resources.register('postgresql-server')
@@ -49,3 +53,19 @@ class PostgresqlServer(ArmResourceManager):
         client = 'PostgreSQLManagementClient'
         enum_spec = ('servers', 'list', None)
         resource_type = 'Microsoft.DBforPostgreSQL/servers'
+
+
+@PostgresqlServer.filter_registry.register('firewall-rules')
+class PostgresqlServerFirewallRulesFilter(FirewallRulesFilter):
+    def _query_rules(self, resource):
+        query = self.client.firewall_rules.list_by_server(
+            resource['resourceGroup'],
+            resource['name'])
+        resource_rules = IPSet()
+        for r in query:
+            rule = IPRange(r.start_ip_address, r.end_ip_address)
+            if rule == AZURE_SERVICES:
+                # Ignore 0.0.0.0 magic value representing Azure Cloud bypass
+                continue
+            resource_rules.add(rule)
+        return resource_rules
--- tools/c7n_azure/tests_azure/tests_resources/test_postgresql_server.py
+++ tools/c7n_azure/tests_azure/tests_resources/test_postgresql_server.py
@@ -1,6 +1,19 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
+import collections
+from netaddr import IPSet
+from mock import Mock
 from ..azure_common import BaseTest, arm_template
+from c7n_azure.resources.postgresql_server import PostgresqlServerFirewallRulesFilter
+
+IpRange = collections.namedtuple('IpRange', 'start_ip_address end_ip_address')
+
+PORTAL_IPS = ['104.42.195.92',
+              '40.76.54.131',
+              '52.176.6.30',
+              '52.169.50.45',
+              '52.187.184.26']
+AZURE_CLOUD_IPS = ['0.0.0.0']
 
 
 class PostgresqlServerTest(BaseTest):
@@ -29,3 +42,33 @@ class PostgresqlServerTest(BaseTest):
         })
         resources = p.run()
         self.assertEqual(len(resources), 1)
+
+
+class PostgresqlServerFirewallFilterTest(BaseTest):
+
+    resource = {'name': 'test', 'resourceGroup': 'test'}
+
+    def test_query_empty_rules(self):
+        rules = []
+        expected = IPSet()
+        self.assertEqual(expected, self._get_filter(rules)._query_rules(self.resource))
+
+    def test_query_regular_rules(self):
+        rules = [IpRange(start_ip_address='10.0.0.0', end_ip_address='10.0.255.255'),
+                 IpRange(start_ip_address='8.8.8.8', end_ip_address='8.8.8.8')]
+        expected = IPSet(['8.8.8.8', '10.0.0.0/16'])
+        self.assertEqual(expected, self._get_filter(rules)._query_rules(self.resource))
+
+    def test_query_regular_rules_with_magic(self):
+        rules = [IpRange(start_ip_address='10.0.0.0', end_ip_address='10.0.255.255'),
+                 IpRange(start_ip_address='8.8.8.8', end_ip_address='8.8.8.8'),
+                 IpRange(start_ip_address='0.0.0.0', end_ip_address='0.0.0.0')]
+        expected = IPSet(['8.8.8.8', '10.0.0.0/16'])
+        self.assertEqual(expected, self._get_filter(rules)._query_rules(self.resource))
+
+    def _get_filter(self, rules, mode='equal'):
+        data = {mode: ['10.0.0.0/8', '127.0.0.1']}
+        filter = PostgresqlServerFirewallRulesFilter(data, Mock())
+        filter.client = Mock()
+        filter.client.firewall_rules.list_by_server.return_value = rules
+        return filter
