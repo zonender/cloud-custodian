ba8569bf40cba90e85d4047eeeddcc6559db102c
h-dub <72996437+h-dub@users.noreply.github.com>
2021-01-08 06:35:32 -0800

    ci - azure - disable flakey functional test (#6388)

--- tools/c7n_azure/tests_azure/tests_resources/test_search.py
+++ tools/c7n_azure/tests_azure/tests_resources/test_search.py
@@ -1,11 +1,13 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
+import pytest
 from ..azure_common import BaseTest, arm_template
 
 
 class SearchTest(BaseTest):
 
     @arm_template('search.json')
+    @pytest.mark.skiplive
     def test_find_by_name(self):
         p = self.load_policy({
             'name': 'test-azure-search',
