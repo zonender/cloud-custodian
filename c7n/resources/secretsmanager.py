ea33b4c9c404df2b6bef7b08c38678045e441910
trastle <trastle@users.noreply.github.com>
2021-05-06 21:35:28 +0800

    aws - secrets manager - tag augmentation fix (#6663)

--- c7n/resources/secretsmanager.py
+++ c7n/resources/secretsmanager.py
@@ -63,7 +63,7 @@ class TagSecretsManagerResource(Tag):
 
     def process_resource_set(self, client, resources, new_tags):
         for r in resources:
-            tags = {t['Key']: t['Value'] for t in r['Tags']}
+            tags = {t['Key']: t['Value'] for t in r.get('Tags', ())}
             for t in new_tags:
                 tags[t['Key']] = t['Value']
             formatted_tags = [{'Key': k, 'Value': v} for k, v in tags.items()]
